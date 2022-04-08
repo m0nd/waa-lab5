61146d21e97732828187ce12e9204859d50c7f4f
Raymond Broome <rbroome@gmail.com>
2022-04-08 14:28:50 -0500

    Update implementations for UserDetails/Service

--- src/main/java/waa/labs/lab5/services/impl/Lab5UserDetails.java
+++ src/main/java/waa/labs/lab5/services/impl/Lab5UserDetails.java
@@ -1,2 +1,64 @@
-package waa.labs.lab5.services.impl;public class Lab5UserDetails {
+package waa.labs.lab5.services.impl;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+import waa.labs.lab5.entities.Role;
+import waa.labs.lab5.entities.User;
+
+import java.util.Collection;
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class Lab5UserDetails implements UserDetails {
+    String email;
+
+    @JsonIgnore
+    String password;
+
+    List<Role> roles;
+
+    public Lab5UserDetails(User user) {
+        this.email = user.getEmail();
+        this.password = user.getPassword();
+        this.roles = user.getRoles();
+    }
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return roles.stream()
+                .map(role -> new SimpleGrantedAuthority(role.getName()))
+                .collect(Collectors.toList());
+    }
+
+    @Override
+    public String getPassword() {
+        return password;
+    }
+
+    @Override
+    public String getUsername() {
+        return email;
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true;
+    }
 }
--- src/main/java/waa/labs/lab5/services/impl/Lab5UserDetailsService.java
+++ src/main/java/waa/labs/lab5/services/impl/Lab5UserDetailsService.java
@@ -1,2 +1,21 @@
-package waa.labs.lab5.services.impl;public class Lab5UserDetailsService {
+package waa.labs.lab5.services.impl;
+
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+import waa.labs.lab5.repositories.IUserRepo;
+
+import javax.transaction.Transactional;
+
+@Service
+@Transactional
+public class Lab5UserDetailsService implements UserDetailsService {
+    IUserRepo userRepo;
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        var user = userRepo.findByEmail();
+        return new Lab5UserDetails(user);
+    }
 }
