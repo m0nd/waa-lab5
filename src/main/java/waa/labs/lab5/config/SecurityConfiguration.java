02013b0e959421953bb4b44bd2a967f5a18e7ae0
Raymond Broome <rbroome@gmail.com>
2022-04-08 23:56:31 -0500

    Set up security configuration

--- src/main/java/waa/labs/lab5/config/SecurityConfiguration.java
+++ src/main/java/waa/labs/lab5/config/SecurityConfiguration.java
@@ -2,16 +2,45 @@ package waa.labs.lab5.config;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import waa.labs.lab5.filters.JwtFilter;
 
 @Configuration
 @EnableWebSecurity
 @RequiredArgsConstructor
 public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
     private final UserDetailsService userDetailsService;
+    private final JwtFilter jwtFilter;
 
+    @Override
+    protected void configure(AuthenticationManagerBuilder authManagerBuilder) throws Exception {
+        // Set the user details service of our authentication manager with our
+        authManagerBuilder.userDetailsService(userDetailsService);
+    }
 
+    @Override
+    protected void configure(HttpSecurity httpSecurity) throws Exception {
+        httpSecurity
+                // Disable CSRF (since it's a RESTful API with no form/form data)
+                .csrf().disable()
+                // Set permissions on endpoints
+                .authorizeRequests()
+                .antMatchers("/api/vi/authenticate/**").permitAll()
+                .antMatchers("/api/v1/admin").hasAuthority("ADMIN")
+                .antMatchers("/api/v1/users").hasAuthority("ADMIN")
+                .antMatchers("/api/v1/posts").hasAuthority("CLIENT")
+                .anyRequest()
+                .authenticated()
+                .and()
+                .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
+        // Apply our JWT filter before Spring's UsernamePasswordAuthenticationFilter
+        httpSecurity.addFilterBefore(jwtFilter, UsernamePasswordAuthenticationFilter.class);
+    }
 }
